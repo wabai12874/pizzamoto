b501074780994db55d510e91f619c7a849062a9c
wabai12874 <brianmaringa19@gmail.com>
2021-06-22 13:16:13 +0300

    finally js is able to calculate the correct amount total before checkout

--- main.js
+++ main.js
@@ -8,10 +8,127 @@ function getOrder(){
     console.log(pizzaFlavour)
     console.log(pizzaSize)
     console.log(pizzaExtras)
-    document.getElementById('crustrow').textContent = pizzaCrust;
-    document.getElementById('flavourrow').textContent = pizzaFlavour;
-    document.getElementById('sizerow').textContent = pizzaSize;
-    document.getElementById('toppingsrow').textContent = pizzaExtras;
+
+    if (pizzaCrust==="Regular"){
+        if (pizzaSize==="Small"){
+            initPrice=200;
+            toppin=500;
+            newprice= initPrice+toppin;
+        }
+        else if (pizzaSize==="Medium"){
+            initPrice=300;
+            toppin=700;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Large"){
+            initPrice=400;
+            toppin=900;
+            newprice= initPrice+toppin;
+
+        }
+
+    }
+
+    else if (pizzaCrust==="Gluten-Free"){
+
+        if (pizzaSize==="Small"){
+            initPrice=250;
+            toppin=500;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Medium"){
+            initPrice=350;
+            toppin=700;
+            newprice= initPrice+toppin;
+
+
+        }
+        else if (pizzaSize==="Large"){
+            initPrice=450;
+            toppin=900;
+            newprice= initPrice+toppin;
+
+        }
+
+
+    }
+    else if(pizzaCrust==="Stuffed"){
+
+        if (pizzaSize==="Small"){
+            initPrice=300;
+            toppin=500;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Medium"){
+            initPrice=400;
+            toppin=700;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Large"){
+            initPrice=500;
+            toppin=900;
+            newprice= initPrice+toppin;
+
+        }
+
+    }
+    else if (pizzaCrust==="Thin-Crispy"){
+
+        if (pizzaSize==="Small"){
+            initPrice=150;
+            toppin=500;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Medium"){
+            initPrice=250;
+            toppin=700;
+            newprice= initPrice+toppin;
+
+        }
+        else if (pizzaSize==="Large"){
+            initPrice=350;
+            toppin=900;
+            newprice= initPrice+toppin;
+
+        }
+
+    }
+
+    if (pizzaExtras==="None"){
+        xCost=0;
+        nPrice=newprice+xCost;
+    }
+    else if (pizzaExtras==="Bacon" || pizzaExtras==="Cheese" || pizzaExtras==="Pepperoni"){
+        xCost=250;
+        nPrice=newprice+xCost;
+    }
+
+
+    document.getElementById('totalrow').textContent = nPrice;
+
+    document.getElementById('crustrow').textContent = pizzaCrust+" ("+pizzaSize+")";
+
+    document.getElementById('flavourrow').textContent = pizzaFlavour +" (at " +toppin+"/=)";
+
+    document.getElementById('sizerow').textContent = pizzaSize+" (at "+initPrice+"/=)";
+
+    document.getElementById('toppingsrow').textContent = pizzaExtras+" (at "+xCost+"/=)";
+
+
+
+
+
+
+
+
+
+
+
 
 
 
